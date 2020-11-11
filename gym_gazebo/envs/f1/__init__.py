cb60d671c549d977271cdb96a1754227f85efc46
igarag <n.arranz.agueda@gmail.com>
2020-11-11 23:14:39 +0100

    envs updated to 'nacho arranz' master thesis

---
+++ gym_gazebo/envs/f1/__init__.py
@@ -0,0 +1,3 @@
+from gym_gazebo.envs.f1.env_gazebo_f1_qlearn_camera   import GazeboF1QlearnCameraEnv
+from gym_gazebo.envs.f1.env_gazebo_f1_qlearn_laser import GazeboF1QlearnLaserEnv
+from gym_gazebo.envs.f1.env_gazebo_f1_dqn_camera import GazeboF1CameraEnvDQN
