e60f67c40c30ef0b9a4c11fd2388800af582a1ba
igarag <n.arranz.agueda@gmail.com>
2020-12-08 20:24:54 +0100

    gazebo_env file improved. Grater importance.

--- gym_gazebo/envs/gazebo_env.py
+++ gym_gazebo/envs/gazebo_env.py
@@ -6,8 +6,10 @@ import os
 import signal
 from pathlib import Path
 
+from gazebo_msgs.msg import ModelState
+from gazebo_msgs.srv import SetModelState, GetModelState
+
 import subprocess
-import time
 from std_srvs.srv import Empty
 import random
 from rosgraph_msgs.msg import Clock
@@ -18,10 +20,10 @@ class GazeboEnv(gym.Env):
     Superclass for all Gazebo environments.
     """
     
-    metadata = {'render.modes': ['human']}
+    metadata = {'render.models': ['human']}
 
     def __init__(self, launchfile):
-
+        print(launchfile)
         self.last_clock_msg = Clock()
         self.port = "11311"  # str(random_number) #os.environ["ROS_PORT_SIM"]
         self.port_gazebo = "11345"  # str(random_number+1) #os.environ["ROS_PORT_SIM"]
@@ -97,6 +99,65 @@ class GazeboEnv(gym.Env):
 
         # Implemented in subclass
         raise NotImplementedError
+
+    def get_position(self):
+        object_coordinates = self.model_coordinates("f1_renault", "")
+        x_position = round(object_coordinates.pose.position.x, 2)
+        y_position = round(object_coordinates.pose.position.y, 2)
+
+        return x_position, y_position
+
+    def _gazebo_reset(self):
+        # Resets the state of the environment and returns an initial observation.
+        rospy.wait_for_service('/gazebo/reset_simulation')
+        try:
+            # reset_proxy.call()
+            self.reset_proxy()
+            self.unpause()
+        except rospy.ServiceException as e:
+            print(f"/gazebo/reset_simulation service call failed: {e}")
+
+    def _gazebo_pause(self):
+        rospy.wait_for_service('/gazebo/pause_physics')
+        try:
+            # resp_pause = pause.call()
+            self.pause()
+        except rospy.ServiceException as e:
+            print(f"/gazebo/pause_physics service call failed: {e}")
+
+    def _gazebo_unpause(self):
+        rospy.wait_for_service('/gazebo/unpause_physics')
+        try:
+            self.unpause()
+        except rospy.ServiceException as e:
+            print(f"/gazebo/unpause_physics service call failed: {e}")
+
+    def _gazebo_set_new_pose(self):
+        """
+        (pos_number, pose_x, pose_y, pose_z, or_x, or_y, or_z, or_z)
+        """
+        pos = random.choice(list(enumerate(self.circuit["gaz_pos"])))[0]
+        self.position = pos
+
+        pos_number = self.circuit["gaz_pos"][0]
+
+        state = ModelState()
+        state.model_name = "f1_renault"
+        state.pose.position.x = self.circuit["gaz_pos"][pos][1]
+        state.pose.position.y = self.circuit["gaz_pos"][pos][2]
+        state.pose.position.z = self.circuit["gaz_pos"][pos][3]
+        state.pose.orientation.x = self.circuit["gaz_pos"][pos][4]
+        state.pose.orientation.y = self.circuit["gaz_pos"][pos][5]
+        state.pose.orientation.z = self.circuit["gaz_pos"][pos][6]
+        state.pose.orientation.w = self.circuit["gaz_pos"][pos][7]
+
+        rospy.wait_for_service('/gazebo/set_model_state')
+        try:
+            set_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
+            set_state(state)
+        except rospy.ServiceException as e:
+            print(f"Service call failed: {e}")
+        return pos_number
 
     def _render(self, mode="human", close=False):
 
