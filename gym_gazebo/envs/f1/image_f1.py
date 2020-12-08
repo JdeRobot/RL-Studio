4a4b776fac028032af9aedd643fc57863e85dfe3
igarag <n.arranz.agueda@gmail.com>
2020-12-08 13:44:27 +0100

    image proceesing functions into a image_f1 file

--- gym_gazebo/envs/f1/image_f1.py
+++ gym_gazebo/envs/f1/image_f1.py
@@ -1,7 +1,11 @@
+import cv2
 import numpy as np
 
 
 class ImageF1:
+
+    font = cv2.FONT_HERSHEY_COMPLEX
+
     def __init__(self):
         self.height = 3  # Image height [pixels]
         self.width = 3  # Image width [pixels]
@@ -14,3 +18,14 @@ class ImageF1:
         return f"Image:" \
                f"\nHeight: {self.height}\nWidth: {self.width}\n" \
                f"Format: {self.format}\nTimeStamp: {self.timeStamp}\nData: {self.data}"
+
+    @staticmethod
+    def image_msg_to_image(img, cv_image):
+
+        image.width = img.width
+        image.height = img.height
+        image.format = "RGB8"
+        image.timeStamp = img.header.stamp.secs + (img.header.stamp.nsecs * 1e-9)
+        image.data = cv_image
+
+        return image
