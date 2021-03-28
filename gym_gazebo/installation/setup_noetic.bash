c37ab22a65ce37e26138744f1e1ec25223962b80
igarag <n.arranz.agueda@gmail.com>
2021-03-28 17:17:27 +0200

    improvements over global installation script

--- gym_gazebo/installation/setup_noetic.bash
+++ gym_gazebo/installation/setup_noetic.bash
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-DIR="../../CustomRobots"
+DIR="../CustomRobots"
 if ! [[ -d "$DIR" ]]
 then
     echo "$DIR doesn't exists. Cloning CustomRobots repository."
-    git clone --branch noetic-devel https://github.com/JdeRobot/CustomRobots.git ../../CustomRobots
-
+    git clone --branch noetic-devel https://github.com/JdeRobot/CustomRobots.git $DIR
 else
     echo "CustomRobots is already downloaded. Pass."
 fi
@@ -22,7 +21,7 @@ fi
 
 source /opt/ros/noetic/setup.bash
 
-# Create catkin_ws
+# ----- Create catkin_ws -----------------------------------
 ws="catkin_ws"
 if [ -d $ws ]; then
   echo "Error: catkin_ws directory already exists" 1>&2
@@ -32,7 +31,6 @@ mkdir -p $src
 cd $src
 catkin_init_workspace
 
-
 # Import and build dependencies
 cd ../../catkin_ws/src/
 vcs import < ../../gazebo_ros_noetic.repos
@@ -41,20 +39,26 @@ cd ..
 touch catkin_ws/src/ecl_navigation/ecl_mobile_robot/CATKIN_IGNORE
 catkin_make
 bash -c 'echo source `pwd`/devel/setup.bash >> ~/.bashrc'
-zsh -c 'echo source `pwd`/devel/setup.bash >> ~/.zshrc'
-echo "## ROS workspace compiled ##"
 
-# add own models path to gazebo models path
+echo "## ROS workspace compiled ##"
+# ---------------------------------
+
+# Add own models path to gazebo models path
 if [ -z "$GAZEBO_MODEL_PATH" ]; then
   bash -c 'echo "export GAZEBO_MODEL_PATH="`pwd`/../CustomRobots/f1/models >> ~/.bashrc'
-  # exec bash #reload bashrc
+else
+  printf "GAZEBO_MODEL_PATH env variable already exists in your .bashrc."
 fi
 
+# ZSH config
 # read -p "Do you use zsh? [Y/n]: " zsh
 # if [ "${!zsh}" = "" ]; then
-  # bash -c 'echo "export GAZEBO_MODEL_PATH="`pwd`/../../CustomRobots/f1/f1 >> ~/.zshrc'
-  # # source $HOME/.zshrc
+  # bash -c 'echo "export GAZEBO_MODEL_PATH="`pwd`/../CustomRobots/f1/models >> ~/.zshrc'
+  # zsh -c 'echo source `pwd`/devel/setup.bash >> ~/.zshrc'
+  # source $HOME/.zshrc
+  
 # fi
 
-# printf ""
-# printf "\nRestart the terminal or type: source ~/.bashrc / .zshrc"
+printf ""
+printf "\nRestarting the terminal...\n"
+exec bash
