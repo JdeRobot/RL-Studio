7691fdb1053825dd8c9563a10ca684ac0f43f85b
igarag <n.arranz.agueda@gmail.com>
2021-07-09 19:41:47 +0200

    :recycle: Move installation folder out of environments directory

--- rl_studio/installation/setup_noetic.bash
+++ rl_studio/installation/setup_noetic.bash
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-DIR="../../CustomRobots"
+source /opt/ros/noetic/setup.bash
+
+# --- CustomRobots ---------------------------------------
+DIR="../CustomRobots"
 if ! [[ -d "$DIR" ]]
 then
     echo "$DIR doesn't exists. Cloning CustomRobots repository."
@@ -13,15 +16,14 @@ if [ -z "$ROS_DISTRO" ]; then
   echo "ROS not installed. Check the installation steps: https://github.com/erlerobot/gym#installing-the-gazebo-environment"
 fi
 
+# --- Gazebo --------------------------------------------
 program="gazebo"
 condition=$(which $program 2>/dev/null | grep -v "not found" | wc -l)
 if [ $condition -eq 0 ] ; then
     echo "Gazebo is not installed. Check the installation steps: https://github.com/erlerobot/gym#installing-the-gazebo-environment"
 fi
 
-source /opt/ros/noetic/setup.bash
-
-# ----- Create catkin_ws -----------------------------------
+# --- Create catkin_ws -----------------------------------
 ws="catkin_ws"
 if [ -d $ws ]; then
   echo "Error: catkin_ws directory already exists" 1>&2
@@ -32,8 +34,8 @@ cd $src
 catkin_init_workspace
 
 # Import and build dependencies
-cd ../../catkin_ws/src/
-vcs import < ../../gazebo_ros_noetic.repos
+cd ../catkin_ws/src/
+vcs import < ../gazebo_ros_noetic.repos
 
 cd ..
 touch catkin_ws/src/ecl_navigation/ecl_mobile_robot/CATKIN_IGNORE
@@ -45,20 +47,27 @@ echo "## ROS workspace compiled ##"
 
 # Add own models path to gazebo models path
 if [ -z "$GAZEBO_MODEL_PATH" ]; then
-  bash -c 'echo "export GAZEBO_MODEL_PATH="`pwd`/../../CustomRobots/f1/models >> ~/.bashrc'
+  bash -c 'echo "export GAZEBO_MODEL_PATH="`pwd`/../CustomRobots/f1/models >> ~/.bashrc'
 else
   printf "GAZEBO_MODEL_PATH env variable already exists in your .bashrc."
 fi
 
+if [ -z "$GAZEBO_RESOURCE_PATH" ]; then
+  bash -c 'echo "export GAZEBO_RESOURCE_PATH=`pwd`/../CustomRobots/f1/worlds >> ~/.bashrc'
+else
+  printf "GAZEBO_RESOURCE_PATH env variable already exist in your .bashrc\n"
+fi
+
 # ZSH config
 # read -p "Do you use zsh? [Y/n]: " zsh
-# if [ "${!zsh}" = "" ]; then
-  # bash -c 'echo "export GAZEBO_MODEL_PATH="`pwd`/../CustomRobots/f1/models >> ~/.zshrc'
-  # zsh -c 'echo source `pwd`/devel/setup.bash >> ~/.zshrc'
-  # source $HOME/.zshrc
-  
-# fi
+#if [ -n "$!zsh" ]; then
+#  zsh -c 'echo "export GAZEBO_MODEL_PATH="`pwd`/../CustomRobots/f1/models >> ~/.zshrc'
+#  zsh -c 'echo "export GAZEBO_RESOURCE_PATH="`pwd`/../CustomRobots/f1/worlds >> ~/.zshrc'
+#  zsh -c 'echo source `pwd`/devel/setup.bash >> ~/.zshrc'
+#  exec zsh
+#fi
 
 printf ""
 printf "\nRestarting the terminal...\n"
+# exec bash
 exec bash
