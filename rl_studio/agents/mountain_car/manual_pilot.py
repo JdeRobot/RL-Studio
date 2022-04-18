52a80744cad12640500873b5b8030245d0f3146a
ruben <ruben.lucas.zaragoza@gmail.com>
2022-04-18 23:41:07 +0200

    simplifying manual pilot

--- rl_studio/agents/mountain_car/manual_pilot.py
+++ rl_studio/agents/mountain_car/manual_pilot.py
@@ -2,22 +2,46 @@ import datetime
 import time
 
 import sys as system
-from envs.gazebo_envs import *
+
+from rl_studio.envs.gazebo_envs import *
 
 import numpy as np
-from functools import reduce
 
-from agents.f1 import settings as settings
+from rl_studio.agents.f1 import settings as settings
 
-# from gym.envs.registration import register
 import utils
 
-# # my envs
-# register(
-#     id='mySim-v0',
-#     entry_point='envs:MyEnv',
-#     # More arguments here
-# )
+
+def execute_steps():
+    highest_reward = 0
+    cumulated_reward = 0
+    for step in range(50000):
+
+        inpt = input("provide action (0-none, 1-left, 2-right): ")
+        if inpt == "1" or inpt == "2" or inpt == "0":
+            action = int(inpt)
+            print("Selected Action!! " + str(action))
+            # Execute the action and get feedback
+            nextState, reward, env.done, lap_completed = env.step(action)
+            cumulated_reward += reward
+
+            if highest_reward < cumulated_reward:
+                highest_reward = cumulated_reward
+            print("cumulated_reward = " + str(cumulated_reward))
+            env._flush(force=True)
+            if env.done:
+                break
+        elif inpt == "q":
+            system.exit(1)
+        else:
+            nextState, reward, env.done, lap_completed = env.step(-1)
+            print("wrong action! Try again")
+            break
+
+        rewards_per_run.append(cumulated_reward)
+        axes.cla()
+        utils.update_line(axes, rewards_per_run)
+
 
 if __name__ == "__main__":
 
@@ -35,8 +59,6 @@ if __name__ == "__main__":
     states_counter = {}
     states_reward = {}
 
-    # plotter = liveplot.LivePlot(outdir)
-
     last_time_steps = np.ndarray(0)
 
     actions = range(env.action_space.n)
@@ -51,65 +73,16 @@ if __name__ == "__main__":
 
     figure, axes = utils.get_stats_figure(rewards_per_run)
 
-    # START ############################################################################################################
     for episode in range(total_episodes):
-
         counter = 0
         lap_completed = False
 
         initial_epsilon = 0.999
-        highest_reward = 0
-        cumulated_reward = 0
+
         print("resetting")
         time.sleep(5)
         state = env.reset()
 
-        # state = ''.join(map(str, observation))
-
-        for step in range(50000):
-
-            inpt = input("provide action (0-none, 1-left, 2-right): ")
-            if inpt == "1" or inpt == "2" or inpt == "0":
-                action = int(inpt)
-                print("Selected Action!! " + str(action))
-                # Execute the action and get feedback
-                nextState, reward, env.done, lap_completed = env.step(action)
-                cumulated_reward += reward
-
-                if highest_reward < cumulated_reward:
-                    highest_reward = cumulated_reward
-                print("cumulated_reward = " + str(cumulated_reward))
-                env._flush(force=True)
-                if not env.done:
-                    state = nextState
-                else:
-                    break
-            elif inpt == "q":
-                system.exit(1)
-            else:
-                nextState, reward, env.done, lap_completed = env.step(-1)
-                print("wrong action! Try again")
-                break
-
-            rewards_per_run.append(cumulated_reward)
-            axes.cla()
-            utils.update_line(axes, rewards_per_run)
-
-    print(
-        "Total EP: {} - epsilon: {} - ep. discount: {} - Highest Reward: {}".format(
-            total_episodes, initial_epsilon, epsilon_discount, highest_reward
-        )
-    )
-
-    l = last_time_steps.tolist()
-    l.sort()
-
-    # print("Parameters: a="+str)
-    print("Overall score: {:0.2f}".format(last_time_steps.mean()))
-    print(
-        "Best 100 score: {:0.2f}".format(
-            reduce(lambda x, y: x + y, l[-100:]) / len(l[-100:])
-        )
-    )
+        execute_steps()
 
     env.close()
