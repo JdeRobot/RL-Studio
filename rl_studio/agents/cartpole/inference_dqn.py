7b5a9ef3a151611b911cb949f484b7e94342a7ec
ruben <ruben.lucas.zaragoza@hotmail.com>
2022-10-05 00:21:26 +0200

    refined cartpole upon new experiments

--- rl_studio/agents/cartpole/inference_dqn.py
+++ rl_studio/agents/cartpole/inference_dqn.py
@@ -108,7 +108,7 @@ class DQNCartpoleInferencer:
                 if random.uniform(0, 1) < self.RANDOM_PERTURBATIONS_LEVEL:
                     perturbation_action = random.randrange(self.env.action_space.n)
                     for perturbation in range(self.PERTURBATIONS_INTENSITY):
-                        self.env.step(perturbation_action)
+                        self.env.perturbate(perturbation_action)
                     logging.info("perturbated in step {} with action {}".format(rew, perturbation_action))
 
                     if rew > 20:
