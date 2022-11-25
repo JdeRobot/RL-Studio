e1684f380b07411776f9f46a7e47dc3444799ca5
sergiopaniego <sergiopaniegoblanco@gmail.com>
2022-11-25 12:29:15 +0100

    Reformatted inference file

--- rl_studio/agents/f1/inference_qlearn.py
+++ rl_studio/agents/f1/inference_qlearn.py
@@ -1,26 +1,22 @@
 import datetime
-import time
 from functools import reduce
 from pprint import pprint
 
 import gym
 import numpy as np
 
-from rl_studio.agents import liveplot
 from rl_studio.agents.f1 import utils
 from rl_studio.agents.f1.settings import QLearnConfig
+from rl_studio.agents.utils import print_messages
 from rl_studio.visual.ascii.images import JDEROBOT_LOGO
 from rl_studio.visual.ascii.text import JDEROBOT, QLEARN_CAMERA, LETS_GO
 from rl_studio.wrappers.inference_rlstudio import InferencerWrapper
 
-from rl_studio.agents.utils import print_messages
-
 
 class F1Inferencer:
     def __init__(self, params):
-        # TODO: Create a pydantic metaclass to simplifyactions thactionse way we extract the params
+        # TODO: Create a pydantic metaclass to simplify actions these way we extract the params
         # environment params
-        self.params = params
         self.environment_params = params.environment["params"]
         self.env_name = params.environment["params"]["env_name"]
         env_params = params.environment["params"]
@@ -28,18 +24,10 @@ class F1Inferencer:
         env_params["actions"] = actions
         self.env = gym.make(self.env_name, **env_params)
         # algorithm params
-        self.alpha = params.algorithm["params"]["alpha"]
-        self.epsilon = params.algorithm["params"]["epsilon"]
-        self.gamma = params.algorithm["params"]["gamma"]
         self.inference_file = params.inference["params"]["inference_file"]
         self.actions_file = params.inference["params"]["actions_file"]
-        # agent
-        # self.action_number = params.agent["params"]["actions_number"]
-        # self.actions_set = params.agent["params"]["actions_set"]
-        # self.actions_values = params.agent["params"]["available_actions"][self.actions_set]
 
     def main(self):
-
         print(JDEROBOT)
         print(JDEROBOT_LOGO)
         print(QLEARN_CAMERA)
@@ -53,8 +41,6 @@ class F1Inferencer:
         states_counter = {}
         states_reward = {}
 
-        plotter = liveplot.LivePlot(outdir)
-
         last_time_steps = np.ndarray(0)
 
         self.actions = range(3)  # range(env.action_space.n)
@@ -63,16 +49,10 @@ class F1Inferencer:
         estimate_step_per_lap = self.environment_params["estimated_steps"]
         lap_completed = False
         total_episodes = 20000
-        epsilon_discount = 0.9986  # Default 0.9986
 
         # TODO: Call the algorithm factory passing "qlearn" as parameter.
-        self.inferencer = InferencerWrapper(
-            "qlearn", self.inference_file, self.actions_file
-        )
+        self.inferencer = InferencerWrapper("qlearn", self.inference_file, self.actions_file)
 
-        highest_reward = 0
-
-        telemetry_start_time = time.time()
         start_time = datetime.datetime.now()
         start_time_format = start_time.strftime("%Y%m%d_%H%M")
 
@@ -85,27 +65,20 @@ class F1Inferencer:
         for episode in range(total_episodes):
 
             counter = 0
-            done = False
             lap_completed = False
-
             cumulated_reward = 0
             observation = env.reset()
-
             state = "".join(map(str, observation))
 
             for step in range(500000):
 
                 counter += 1
-
                 # Pick an action based on the current state
                 action = self.inferencer.inference(state)
 
                 # Execute the action and get feedback
                 observation, reward, done, info = env.step(action)
                 cumulated_reward += reward
-
-                if highest_reward < cumulated_reward:
-                    highest_reward = cumulated_reward
 
                 nextState = "".join(map(str, observation))
 
@@ -130,9 +103,9 @@ class F1Inferencer:
                         )
 
                     if (
-                        datetime.datetime.now()
-                        - datetime.timedelta(minutes=3, seconds=12)
-                        > start_time
+                            datetime.datetime.now()
+                            - datetime.timedelta(minutes=3, seconds=12)
+                            > start_time
                     ):
                         print("Finish. Saving parameters . . .")
                         utils.save_times(checkpoints)
@@ -153,16 +126,12 @@ class F1Inferencer:
 
                 if step > estimate_step_per_lap and not lap_completed:
                     lap_completed = True
-                    if config.plotter_graphic:
-                        plotter.plot_steps_vs_epoch(stats, save=True)
                     print(
                         f"\n\n====> LAP COMPLETED in: {datetime.datetime.now() - start_time} - Epoch: {episode}"
                         f" - Cum. Reward: {cumulated_reward} <====\n\n"
                     )
 
                 if counter > 1000:
-                    if config.plotter_graphic:
-                        plotter.plot_steps_vs_epoch(stats, save=True)
                     counter = 0
 
                 if datetime.datetime.now() - datetime.timedelta(hours=2) > start_time:
@@ -173,62 +142,36 @@ class F1Inferencer:
                     env.close()
                     exit(0)
 
-            if episode % 1 == 0 and config.plotter_graphic:
-                # plotter.plot(env)
-                plotter.plot_steps_vs_epoch(stats)
-                # plotter.full_plot(env, stats, 2)  # optional parameter = mode (0, 1, 2)
-
-            m, s = divmod(int(time.time() - telemetry_start_time), 60)
-            h, m = divmod(m, 60)
-
-        l = last_time_steps.tolist()
-        l.sort()
+        last_time_steps_list = last_time_steps.tolist()
+        last_time_steps_list.sort()
 
         print("Overall score: {:0.2f}".format(last_time_steps.mean()))
         print(
             "Best 100 score: {:0.2f}".format(
-                reduce(lambda x, y: x + y, l[-100:]) / len(l[-100:])
+                reduce(lambda x, y: x + y, last_time_steps_list[-100:]) / len(last_time_steps_list[-100:])
             )
         )
-
-        plotter.plot_steps_vs_epoch(stats, save=True)
 
         env.close()
 
 
-        
 class QlearnF1FollowLaneInferencer:
     def __init__(self, params):
-        # TODO: Create a pydantic metaclass to simplifyactions thactionse way we extract the params
+        # TODO: Create a pydantic metaclass to simplify actions these way we extract the params
         # environment params
-        self.params = params
         self.environment_params = params.environment["params"]
         self.env_name = params.environment["params"]["env_name"]
-        env_params = params.environment["params"]
-        actions = params.environment["actions"]
-        env_params["actions"] = actions
-        
+
         # algorithm params
-        self.alpha = params.algorithm["params"]["alpha"]
-        self.epsilon = params.algorithm["params"]["epsilon"]
-        self.gamma = params.algorithm["params"]["gamma"]
         self.inference_file = params.inference["params"]["inference_file"]
         self.actions_file = params.inference["params"]["actions_file"]
-        
-        
+
         # States
         self.state_space = params.agent["params"]["states"]["state_space"]
-        self.states = params.agent["params"]["states"]
-
-        # Actions
-        self.action_space = params.environment["actions_set"]
-        self.actions = params.environment["actions"]
-        self.actions_size = params.environment["actions_number"]
 
         # Rewards
         self.reward_function = params.agent["params"]["rewards"]["reward_function"]
-        self.min_reward = params.agent["params"]["rewards"][self.reward_function]["min_reward"]
-        
+
         # Agent
         self.environment = {}
         self.environment["agent"] = params.agent["name"]
@@ -269,7 +212,7 @@ class QlearnF1FollowLaneInferencer:
         self.environment["ROS_MASTER_URI"] = params.settings["params"]["ros_master_uri"]
         self.environment["GAZEBO_MASTER_URI"] = params.settings["params"]["gazebo_master_uri"]
         self.environment["telemetry"] = params.settings["params"]["telemetry"]
-        
+
         print_messages(
             "environment",
             environment=self.environment,
@@ -277,11 +220,6 @@ class QlearnF1FollowLaneInferencer:
 
         # Env
         self.env = gym.make(self.env_name, **self.environment)
-        
-        # agent
-        # self.action_number = params.agent["params"]["actions_number"]
-        # self.actions_set = params.agent["params"]["actions_set"]
-        # self.actions_values = params.agent["params"]["available_actions"][self.actions_set]
 
     def main(self):
 
@@ -298,8 +236,6 @@ class QlearnF1FollowLaneInferencer:
         states_counter = {}
         states_reward = {}
 
-        plotter = liveplot.LivePlot(outdir)
-
         last_time_steps = np.ndarray(0)
 
         self.actions = range(3)  # range(env.action_space.n)
@@ -308,16 +244,12 @@ class QlearnF1FollowLaneInferencer:
         estimate_step_per_lap = self.environment_params["estimated_steps"]
         lap_completed = False
         total_episodes = 20000
-        epsilon_discount = 0.9986  # Default 0.9986
 
         # TODO: Call the algorithm factory passing "qlearn" as parameter.
         self.inferencer = InferencerWrapper(
             "qlearn", self.inference_file, self.actions_file
         )
 
-        highest_reward = 0
-
-        telemetry_start_time = time.time()
         start_time = datetime.datetime.now()
         start_time_format = start_time.strftime("%Y%m%d_%H%M")
 
@@ -330,7 +262,6 @@ class QlearnF1FollowLaneInferencer:
         for episode in range(total_episodes):
 
             counter = 0
-            done = False
             lap_completed = False
 
             cumulated_reward = 0
@@ -348,9 +279,6 @@ class QlearnF1FollowLaneInferencer:
                 # Execute the action and get feedback
                 observation, reward, done, info = env.step(action)
                 cumulated_reward += reward
-
-                if highest_reward < cumulated_reward:
-                    highest_reward = cumulated_reward
 
                 nextState = "".join(map(str, observation))
 
@@ -375,9 +303,9 @@ class QlearnF1FollowLaneInferencer:
                         )
 
                     if (
-                        datetime.datetime.now()
-                        - datetime.timedelta(minutes=3, seconds=12)
-                        > start_time
+                            datetime.datetime.now()
+                            - datetime.timedelta(minutes=3, seconds=12)
+                            > start_time
                     ):
                         print("Finish. Saving parameters . . .")
                         utils.save_times(checkpoints)
@@ -398,16 +326,12 @@ class QlearnF1FollowLaneInferencer:
 
                 if step > estimate_step_per_lap and not lap_completed:
                     lap_completed = True
-                    if config.plotter_graphic:
-                        plotter.plot_steps_vs_epoch(stats, save=True)
                     print(
                         f"\n\n====> LAP COMPLETED in: {datetime.datetime.now() - start_time} - Epoch: {episode}"
                         f" - Cum. Reward: {cumulated_reward} <====\n\n"
                     )
 
                 if counter > 1000:
-                    if config.plotter_graphic:
-                        plotter.plot_steps_vs_epoch(stats, save=True)
                     counter = 0
 
                 if datetime.datetime.now() - datetime.timedelta(hours=2) > start_time:
@@ -418,24 +342,14 @@ class QlearnF1FollowLaneInferencer:
                     env.close()
                     exit(0)
 
-            if episode % 1 == 0 and config.plotter_graphic:
-                # plotter.plot(env)
-                plotter.plot_steps_vs_epoch(stats)
-                # plotter.full_plot(env, stats, 2)  # optional parameter = mode (0, 1, 2)
-
-            m, s = divmod(int(time.time() - telemetry_start_time), 60)
-            h, m = divmod(m, 60)
-
-        l = last_time_steps.tolist()
-        l.sort()
+        last_time_steps_list = last_time_steps.tolist()
+        last_time_steps_list.sort()
 
         print("Overall score: {:0.2f}".format(last_time_steps.mean()))
         print(
             "Best 100 score: {:0.2f}".format(
-                reduce(lambda x, y: x + y, l[-100:]) / len(l[-100:])
+                reduce(lambda x, y: x + y, last_time_steps_list[-100:]) / len(last_time_steps_list[-100:])
             )
         )
-
-        plotter.plot_steps_vs_epoch(stats, save=True)
 
         env.close()
