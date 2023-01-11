5dc7840e70082f540b44eb5bb41424efff77f51b
pjfernandecabo <pedro.fdc@gmail.com>
2023-01-11 20:28:06 +0000

    V12: follow lane ddpg training, retraining and inference

---
+++ rl_studio/agents/f1/train_followlane_ddpg_f1_gazebo_tf.py
@@ -0,0 +1,362 @@
+from datetime import datetime, timedelta
+import os
+import random
+import time
+
+import gymnasium as gym
+import numpy as np
+import tensorflow as tf
+from tqdm import tqdm
+
+from rl_studio.agents.f1.loaders import (
+    LoadAlgorithmParams,
+    LoadEnvParams,
+    LoadEnvVariablesDDPGGazebo,
+    LoadGlobalParams,
+)
+from rl_studio.agents.utils import (
+    print_messages,
+    render_params,
+    save_dataframe_episodes,
+    LoggingHandler,
+)
+from rl_studio.algorithms.ddpg import (
+    ModifiedTensorBoard,
+    OUActionNoise,
+    Buffer,
+    DDPGAgent,
+)
+from rl_studio.algorithms.utils import (
+    save_actorcritic_model,
+)
+from rl_studio.envs.gazebo.gazebo_envs import *
+
+
+class TrainerFollowLaneDDPGF1GazeboTF:
+    """
+    Mode: training
+    Task: Follow Lane
+    Algorithm: DDPG
+    Agent: F1
+    Simulator: Gazebo
+    Framework: TensorFlow
+    """
+
+    def __init__(self, config):
+        self.algoritmhs_params = LoadAlgorithmParams(config)
+        self.env_params = LoadEnvParams(config)
+        self.environment = LoadEnvVariablesDDPGGazebo(config)
+        self.global_params = LoadGlobalParams(config)
+
+        os.makedirs(f"{self.global_params.models_dir}", exist_ok=True)
+        os.makedirs(f"{self.global_params.logs_dir}", exist_ok=True)
+        os.makedirs(f"{self.global_params.metrics_data_dir}", exist_ok=True)
+        os.makedirs(f"{self.global_params.metrics_graphics_dir}", exist_ok=True)
+        self.log_file = f"{self.global_params.logs_dir}/{time.strftime('%Y%m%d-%H%M%S')}_{self.global_params.mode}_{self.global_params.task}_{self.global_params.algorithm}_{self.global_params.agent}_{self.global_params.framework}.log"
+
+    def main(self):
+
+        log = LoggingHandler(self.log_file)
+
+        ## Load Environment
+        env = gym.make(self.env_params.env_name, **self.environment.environment)
+
+        random.seed(1)
+        np.random.seed(1)
+        tf.compat.v1.random.set_random_seed(1)
+
+        start_time = datetime.now()
+        best_epoch = 1
+        current_max_reward = 0
+        best_step = 0
+        best_epoch_training_time = 0
+        ## Reset env
+        state, state_size = env.reset()
+
+        log.logger.info(
+            f"\nstates = {self.global_params.states}\n"
+            f"states_set = {self.global_params.states_set}\n"
+            f"states_len = {len(self.global_params.states_set)}\n"
+            f"actions = {self.global_params.actions}\n"
+            f"actions set = {self.global_params.actions_set}\n"
+            f"actions_len = {len(self.global_params.actions_set)}\n"
+            f"actions_range = {range(len(self.global_params.actions_set))}\n"
+            f"batch_size = {self.algoritmhs_params.batch_size}\n"
+            f"logs_tensorboard_dir = {self.global_params.logs_tensorboard_dir}\n"
+        )
+        ## --------------------- Deep Nets ------------------
+        ou_noise = OUActionNoise(
+            mean=np.zeros(1),
+            std_deviation=float(self.algoritmhs_params.std_dev) * np.ones(1),
+        )
+        # Init Agents
+        ac_agent = DDPGAgent(
+            self.environment.environment,
+            len(self.global_params.actions_set),
+            state_size,
+            self.global_params.models_dir,
+        )
+        # init Buffer
+        buffer = Buffer(
+            state_size,
+            len(self.global_params.actions_set),
+            self.global_params.states,
+            self.global_params.actions,
+            self.algoritmhs_params.buffer_capacity,
+            self.algoritmhs_params.batch_size,
+        )
+        # Init TensorBoard
+        tensorboard = ModifiedTensorBoard(
+            log_dir=f"{self.global_params.logs_tensorboard_dir}/{self.algoritmhs_params.model_name}-{time.strftime('%Y%m%d-%H%M%S')}"
+        )
+        # show rewards stats per episode
+
+        ## -------------    START TRAINING --------------------
+        for episode in tqdm(
+            range(1, self.env_params.total_episodes + 1), ascii=True, unit="episodes"
+        ):
+            tensorboard.step = episode
+            done = False
+            cumulated_reward = 0
+            step = 1
+            start_time_epoch = datetime.now()
+
+            prev_state, prev_state_size = env.reset()
+
+            while not done:
+                tf_prev_state = tf.expand_dims(tf.convert_to_tensor(prev_state), 0)
+                action = ac_agent.policy(
+                    tf_prev_state, ou_noise, self.global_params.actions
+                )
+                state, reward, done, _ = env.step(action, step)
+                cumulated_reward += reward
+
+                # learn and update
+                buffer.record((prev_state, action, reward, state))
+                buffer.learn(ac_agent, self.algoritmhs_params.gamma)
+                ac_agent.update_target(
+                    ac_agent.target_actor.variables,
+                    ac_agent.actor_model.variables,
+                    self.algoritmhs_params.tau,
+                )
+                ac_agent.update_target(
+                    ac_agent.target_critic.variables,
+                    ac_agent.critic_model.variables,
+                    self.algoritmhs_params.tau,
+                )
+
+                #
+                prev_state = state
+                step += 1
+
+                log.logger.debug(
+                    f"\nstate = {state}\n"
+                    f"state type = {type(state)}\n"
+                    f"prev_state = {prev_state}\n"
+                    f"prev_state = {type(prev_state)}\n"
+                    f"action = {action}\n"
+                    f"actions type = {type(action)}\n"
+                )
+                render_params(
+                    task=self.global_params.task,
+                    # v=action[0][0],  # for continuous actions
+                    # w=action[0][1],  # for continuous actions
+                    episode=episode,
+                    step=step,
+                    state=state,
+                    # v=self.global_params.actions_set[action][
+                    #    0
+                    # ],  # this case for discrete
+                    # w=self.global_params.actions_set[action][
+                    #    1
+                    # ],  # this case for discrete
+                    reward_in_step=reward,
+                    cumulated_reward_in_this_episode=cumulated_reward,
+                    _="--------------------------",
+                    best_episode_until_now=best_epoch,
+                    in_best_step=best_step,
+                    with_highest_reward=int(current_max_reward),
+                    in_best_epoch_training_time=best_epoch_training_time,
+                )
+                log.logger.debug(
+                    f"\nepisode = {episode}\n"
+                    f"step = {step}\n"
+                    f"actions_len = {len(self.global_params.actions_set)}\n"
+                    f"actions_range = {range(len(self.global_params.actions_set))}\n"
+                    f"actions = {self.global_params.actions_set}\n"
+                    f"reward_in_step = {reward}\n"
+                    f"cumulated_reward = {cumulated_reward}\n"
+                    f"done = {done}\n"
+                )
+                # best episode
+                if current_max_reward <= cumulated_reward:
+                    current_max_reward = cumulated_reward
+                    best_epoch = episode
+                    best_step = step
+                    best_epoch_training_time = datetime.now() - start_time_epoch
+                    # saving params to show
+                    self.global_params.actions_rewards["episode"].append(episode)
+                    self.global_params.actions_rewards["step"].append(step)
+                    # For continuous actios
+                    # self.actions_rewards["v"].append(action[0][0])
+                    # self.actions_rewards["w"].append(action[0][1])
+                    self.global_params.actions_rewards["reward"].append(reward)
+                    self.global_params.actions_rewards["center"].append(
+                        env.image_center
+                    )
+
+                # Showing stats in screen for monitoring. Showing every 'save_every_step' value
+                if not step % self.env_params.save_every_step:
+                    save_dataframe_episodes(
+                        self.environment.environment,
+                        self.global_params.metrics_data_dir,
+                        self.global_params.aggr_ep_rewards,
+                        self.global_params.actions_rewards,
+                    )
+                    log.logger.debug(
+                        f"SHOWING BATCH OF STEPS\n"
+                        f"current_max_reward = {cumulated_reward}\n"
+                        f"current epoch = {episode}\n"
+                        f"current step = {step}\n"
+                        f"best epoch so far = {best_epoch}\n"
+                        f"best step so far = {best_step}\n"
+                        f"best_epoch_training_time = {best_epoch_training_time}\n"
+                    )
+
+                #####################################################
+                ### save in case of completed steps in one episode
+                if step >= self.env_params.estimated_steps:
+                    done = True
+                    log.logger.info(
+                        f"\nEPISODE COMPLETED\n"
+                        f"in episode = {episode}\n"
+                        f"steps = {step}\n"
+                        f"cumulated_reward = {cumulated_reward}\n"
+                    )
+                    save_actorcritic_model(
+                        ac_agent,
+                        self.global_params,
+                        self.algoritmhs_params,
+                        cumulated_reward,
+                        episode,
+                        "LAPCOMPLETED",
+                    )
+            #####################################################
+            #### save best lap in episode
+            if (
+                cumulated_reward - self.environment.environment["rewards"]["penal"]
+            ) >= current_max_reward and episode > 1:
+
+                self.global_params.best_current_epoch["best_epoch"].append(best_epoch)
+                self.global_params.best_current_epoch["highest_reward"].append(
+                    current_max_reward
+                )
+                self.global_params.best_current_epoch["best_step"].append(best_step)
+                self.global_params.best_current_epoch[
+                    "best_epoch_training_time"
+                ].append(best_epoch_training_time)
+                self.global_params.best_current_epoch[
+                    "current_total_training_time"
+                ].append(datetime.now() - start_time)
+
+                save_dataframe_episodes(
+                    self.environment.environment,
+                    self.global_params.metrics_data_dir,
+                    self.global_params.best_current_epoch,
+                )
+                save_actorcritic_model(
+                    ac_agent,
+                    self.global_params,
+                    self.algoritmhs_params,
+                    cumulated_reward,
+                    episode,
+                    "BESTLAP",
+                )
+                log.logger.info(
+                    f"\nsaving best lap\n"
+                    f"in episode = {episode}\n"
+                    f"current_max_reward = {cumulated_reward}\n"
+                    f"steps = {step}\n"
+                )
+            #####################################################
+            ### end episode in time settings: 2 hours, 15 hours...
+            if (
+                datetime.now() - timedelta(hours=self.global_params.training_time)
+                > start_time
+            ) or (episode > self.env_params.total_episodes):
+                log.logger.info(
+                    f"\nTraining Time over\n"
+                    f"current_max_reward = {cumulated_reward}\n"
+                    f"epoch = {episode}\n"
+                    f"step = {step}\n"
+                )
+                if cumulated_reward > current_max_reward:
+                    save_actorcritic_model(
+                        ac_agent,
+                        self.global_params,
+                        self.algoritmhs_params,
+                        cumulated_reward,
+                        episode,
+                        "FINISHTIME",
+                    )
+
+                break
+
+            #####################################################
+            ### save every save_episode times
+            self.global_params.ep_rewards.append(cumulated_reward)
+            if not episode % self.env_params.save_episodes:
+                average_reward = sum(
+                    self.global_params.ep_rewards[-self.env_params.save_episodes :]
+                ) / len(self.global_params.ep_rewards[-self.env_params.save_episodes :])
+                min_reward = min(
+                    self.global_params.ep_rewards[-self.env_params.save_episodes :]
+                )
+                max_reward = max(
+                    self.global_params.ep_rewards[-self.env_params.save_episodes :]
+                )
+                tensorboard.update_stats(
+                    reward_avg=int(average_reward),
+                    reward_max=int(max_reward),
+                    steps=step,
+                )
+                self.global_params.aggr_ep_rewards["episode"].append(episode)
+                self.global_params.aggr_ep_rewards["step"].append(step)
+                self.global_params.aggr_ep_rewards["avg"].append(average_reward)
+                self.global_params.aggr_ep_rewards["max"].append(max_reward)
+                self.global_params.aggr_ep_rewards["min"].append(min_reward)
+                self.global_params.aggr_ep_rewards["epoch_training_time"].append(
+                    (datetime.now() - start_time_epoch).total_seconds()
+                )
+                self.global_params.aggr_ep_rewards["total_training_time"].append(
+                    (datetime.now() - start_time).total_seconds()
+                )
+                save_actorcritic_model(
+                    ac_agent,
+                    self.global_params,
+                    self.algoritmhs_params,
+                    cumulated_reward,
+                    episode,
+                    "BATCH",
+                )
+
+                save_dataframe_episodes(
+                    self.environment.environment,
+                    self.global_params.metrics_data_dir,
+                    self.global_params.aggr_ep_rewards,
+                )
+                log.logger.info(
+                    f"\nsaving BATCH\n"
+                    f"current_max_reward = {cumulated_reward}\n"
+                    f"best_epoch = {best_epoch}\n"
+                    f"best_step = {best_step}\n"
+                    f"best_epoch_training_time = {best_epoch_training_time}\n"
+                )
+        #####################################################
+        ### save last episode, not neccesarily the best one
+        save_dataframe_episodes(
+            self.environment.environment,
+            self.global_params.metrics_data_dir,
+            self.global_params.aggr_ep_rewards,
+        )
+        env.close()
---
+++ rl_studio/envs/gazebo/f1/models/followlane_ddpg_tf.py
@@ -0,0 +1,249 @@
+#############################################
+# - Task: Follow Lane
+# - Algorithm: DDPG
+# - actions: discrete and continuous
+# - State: Simplified perception and raw image
+#
+############################################
+
+import math
+
+from cv_bridge import CvBridge
+import cv2
+from geometry_msgs.msg import Twist
+import numpy as np
+
+import rospy
+from sensor_msgs.msg import Image
+
+from rl_studio.agents.utils import (
+    print_messages,
+)
+from rl_studio.envs.gazebo.f1.image_f1 import ImageF1
+from rl_studio.envs.gazebo.f1.models.images import F1GazeboImages
+from rl_studio.envs.gazebo.f1.models.f1_env import F1Env
+from rl_studio.envs.gazebo.f1.models.utils import F1GazeboUtils
+from rl_studio.envs.gazebo.f1.models.rewards import F1GazeboRewards
+from rl_studio.envs.gazebo.f1.models.simplified_perception import (
+    F1GazeboSimplifiedPerception,
+)
+
+
+class FollowLaneDDPGF1GazeboTF(F1Env):
+    def __init__(self, **config):
+
+        F1Env.__init__(self, **config)
+        self.simplifiedperception = F1GazeboSimplifiedPerception()
+        self.f1gazeborewards = F1GazeboRewards()
+        self.f1gazeboutils = F1GazeboUtils()
+        self.f1gazeboimages = F1GazeboImages()
+
+        self.image = ImageF1()
+        self.image_raw_from_topic = None
+        self.f1_image_camera = None
+        self.sensor = config["sensor"]
+
+        # Image
+        self.image_resizing = config["image_resizing"] / 100
+        self.new_image_size = config["new_image_size"]
+        self.raw_image = config["raw_image"]
+        self.height = int(config["height_image"] * self.image_resizing)
+        self.width = int(config["width_image"] * self.image_resizing)
+        self.center_image = int(config["center_image"] * self.image_resizing)
+        self.num_regions = config["num_regions"]
+        self.pixel_region = int(self.center_image / self.num_regions) * 2
+        self.telemetry_mask = config["telemetry_mask"]
+        self.poi = config["x_row"][0]
+        self.image_center = None
+        self.right_lane_center_image = config["center_image"] + (
+            config["center_image"] // 2
+        )
+        self.lower_limit = config["lower_limit"]
+
+        # States
+        self.state_space = config["states"]
+        if self.state_space == "spn":
+            self.x_row = [i for i in range(1, int(self.height / 2) - 1)]
+        else:
+            self.x_row = config["x_row"]
+
+        # Actions
+        self.action_space = config["action_space"]
+        self.actions = config["actions"]
+
+        # Rewards
+        self.reward_function = config["reward_function"]
+        self.rewards = config["rewards"]
+        self.min_reward = config["min_reward"]
+        if self.action_space == "continuous":
+            self.beta_1 = self.actions["w"][1] / (
+                self.actions["v"][1] - self.actions["v"][0]
+            )
+            self.beta_0 = self.beta_1 * self.actions["v"][1]
+
+        # Others
+        self.telemetry = config["telemetry"]
+
+        print_messages(
+            "FollowLaneDDPGF1GazeboTF()",
+            actions=self.actions,
+            len_actions=len(self.actions),
+            # actions_v=self.actions["v"], # for continuous actions
+            # actions_v=self.actions[0], # for discrete actions
+            # beta_1=self.beta_1,
+            # beta_0=self.beta_0,
+            rewards=self.rewards,
+        )
+
+    #################################################################################
+    # reset
+    #################################################################################
+
+    def reset(self):
+        """
+        Main reset. Depending of:
+        - sensor
+        - states: images or simplified perception (sp)
+
+        """
+        if self.sensor == "camera":
+            return self.reset_camera()
+
+    def reset_camera(self):
+        self._gazebo_reset()
+        # === POSE ===
+        if self.alternate_pose:
+            self._gazebo_set_random_pose_f1_follow_rigth_lane()
+        else:
+            self._gazebo_set_fix_pose_f1_follow_right_lane()
+
+        self._gazebo_unpause()
+
+        ##==== get image from sensor camera
+        f1_image_camera, _ = self.get_camera_info()
+        self._gazebo_pause()
+
+        ##==== calculating State
+        # image as observation
+        if self.state_space == "image":
+            state = np.array(
+                self.f1gazeboimages.image_preprocessing_black_white_32x32(
+                    f1_image_camera.data, self.height
+                )
+            )
+            state_size = state.shape
+
+        # simplified perception as observation
+        else:
+            (
+                centrals_in_pixels,
+                centrals_normalized,
+            ) = self.simplifiedperception.calculate_centrals_lane(
+                f1_image_camera.data,
+                self.height,
+                self.width,
+                self.x_row,
+                self.lower_limit,
+                self.center_image,
+            )
+            states = self.simplifiedperception.calculate_observation(
+                centrals_in_pixels, self.center_image, self.pixel_region
+            )
+            state = [states[0]]
+            state_size = len(state)
+
+        return state, state_size
+
+    #################################################################################
+    # Camera
+    #################################################################################
+    def get_camera_info(self):
+        image_data = None
+        f1_image_camera = None
+        success = False
+
+        while image_data is None or success is False:
+            image_data = rospy.wait_for_message(
+                "/F1ROS/cameraL/image_raw", Image, timeout=5
+            )
+            cv_image = CvBridge().imgmsg_to_cv2(image_data, "bgr8")
+            f1_image_camera = self.image_msg_to_image(image_data, cv_image)
+            # f1_image_camera = image_msg_to_image(image_data, cv_image)
+            if np.any(cv_image):
+                success = True
+
+        return f1_image_camera, cv_image
+
+    def image_msg_to_image(self, img, cv_image):
+        self.image.width = img.width
+        self.image.height = img.height
+        self.image.format = "RGB8"
+        self.image.timeStamp = img.header.stamp.secs + (img.header.stamp.nsecs * 1e-9)
+        self.image.data = cv_image
+
+        return self.image
+
+    #################################################################################
+    # step
+    #################################################################################
+
+    def step(self, action, step):
+        self._gazebo_unpause()
+        vel_cmd = Twist()
+
+        if self.action_space == "continuous":
+            vel_cmd.linear.x = action[0][0]
+            vel_cmd.angular.z = action[0][1]
+        else:
+            vel_cmd.linear.x = self.actions[action][0]
+            vel_cmd.angular.z = self.actions[action][1]
+
+        self.vel_pub.publish(vel_cmd)
+
+        ##==== get image from sensor camera
+        f1_image_camera, _ = self.get_camera_info()
+        self._gazebo_pause()
+
+        ##==== get center
+        points, centrals_normalized = self.simplifiedperception.processed_image(
+            f1_image_camera.data, self.height, self.width, self.x_row, self.center_image
+        )
+        if self.state_space == "spn":
+            self.point = points[self.poi]
+        else:
+            self.point = points[0]
+        # center = abs(float(self.center_image - self.point) / (float(self.width) // 2))
+        center = float(self.center_image - self.point) / (float(self.width) // 2)
+
+        ##==== get State
+        ##==== image as observation
+        if self.state_space == "image":
+            state = np.array(
+                self.f1gazeboimages.image_preprocessing_black_white_32x32(
+                    f1_image_camera.data, self.height
+                )
+            )
+
+        ##==== simplified perception as observation
+        else:
+            state = self.simplifiedperception.calculate_observation(
+                points, self.center_image, self.pixel_region
+            )
+
+        ##==== get Rewards
+        if self.reward_function == "follow_right_lane_center_v_step":
+            reward, done = self.f1gazeborewards.rewards_followlane_v_centerline_step(
+                vel_cmd, center, step, self.rewards
+            )
+        elif (
+            self.reward_function == "follow_right_lane_center_v_w_linear"
+        ):  # this reward function ONLY for continuous actions
+            reward, done = self.f1gazeborewards.rewards_followlane_v_w_centerline(
+                vel_cmd, center, self.rewards, self.beta_1, self.beta_0
+            )
+        else:
+            reward, done = self.f1gazeborewards.rewards_followlane_centerline(
+                center, self.rewards
+            )
+
+        return state, reward, done, {}
